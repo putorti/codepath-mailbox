21e0a329330154ade5706e78b1d0f3919d299911
Jason Putorti <jason@brigade.com>
2016-02-20 21:42:45 -0800

    Icon/color logic is working for the message item

--- mailbox/MailboxViewController.swift
+++ mailbox/MailboxViewController.swift
@@ -16,14 +16,24 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet var messagePan: UIPanGestureRecognizer!
     @IBOutlet var edgePan: UIScreenEdgePanGestureRecognizer!
     @IBOutlet weak var mainUI: UIView!
+    @IBOutlet weak var leftIcon: UIImageView!
+    @IBOutlet weak var rightIcon: UIImageView!
+    
+    var messageOriginalColor: UIColor!
     var messageOriginalCenter: CGPoint!
     var messageCenter: CGPoint!
+    var mainUIOriginalCenter: CGPoint!
+    var mainUICenter: CGPoint!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         feedScroller.delegate = self
         feedScroller.contentSize = feedImage.image!.size
         messageOriginalCenter = messageView.center
+        mainUIOriginalCenter = mainUI.center
+        messageOriginalColor = messageView.backgroundColor!
+        
         // Do any additional setup after loading the view, typically from a nib.
         
         // The onCustomPan: method will be defined in Step 3 below.
@@ -55,13 +65,36 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
             print("Gesture began at: \(point)")
         } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
             print("Gesture changed at: \(point)")
-            
             messageView.center = CGPoint(x: messageOriginalCenter.x + translation.x, y: messageOriginalCenter.y) // allow drag on x-axis only
+            print("frame X: \(messageView.frame.origin.x)")
+            
+            if messageView.frame.origin.x < -320 { // pulled left
+                if messageView.frame.origin.x < -530 {
+                    messageView.backgroundColor = UIColor(red: 0.81, green: 0.76, blue: 0.15, alpha: 1)
+                    rightIcon.image = UIImage(named: "list_icon")
+                    print("menu")
+                } else {
+                    messageView.backgroundColor = UIColor(red: 0.95, green: 0.95, blue: 0, alpha: 1)
+                    rightIcon.image = UIImage(named: "later_icon")
+                    print("later")
+                }
+            } else { // pulled right
+                print("pulled right")
+                if messageView.frame.origin.x > -120 {
+                    messageView.backgroundColor = UIColor(red: 0.95, green: 0, blue: 0, alpha: 1)
+                    leftIcon.image = UIImage(named: "delete_icon")
+                    print("delete")
+                } else {
+                    messageView.backgroundColor = UIColor(red: 0, green: 0.95, blue: 0, alpha: 1)
+                    leftIcon.image = UIImage(named: "archive_icon")
+                    print("archive")
+                }
+            }
             
         } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
             print("Gesture ended at: \(point)")
-            
             messageView.center = messageOriginalCenter // snap back to origin
+            messageView.backgroundColor = messageOriginalColor
         }
     }
     
@@ -78,9 +111,11 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
             print("Edge gesture began at: \(point)")
         } else if edgeGestureRecognizer.state == UIGestureRecognizerState.Changed {
             print("Edge gesture changed at: \(point)")
+            mainUI.center = CGPoint(x: mainUIOriginalCenter.x + translation.x, y: mainUIOriginalCenter.y) // allow drag on x-axis only
             
         } else if edgeGestureRecognizer.state == UIGestureRecognizerState.Ended {
             print("Edge gesture ended at: \(point)")
+            mainUI.center = mainUIOriginalCenter // snap back to origin
         }
         
     }
