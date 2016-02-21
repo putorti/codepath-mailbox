8626209e7c65de7ffa0f0b3dcacc242a7de1caaa
Jason Putorti <jason@brigade.com>
2016-02-20 19:50:00 -0800

    Dragging is enabled now on the message row
    Still no snapping or color changes

--- mailbox/ViewController.swift
+++ mailbox/ViewController.swift
@@ -12,19 +12,52 @@ class ViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var feedScroller: UIScrollView!
     @IBOutlet weak var feedImage: UIImageView!
+    @IBOutlet weak var messageView: UIView!
+    @IBOutlet var messagePan: UIPanGestureRecognizer!
+    var messageOriginalCenter: CGPoint!
+    var messageCenter: CGPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         feedScroller.delegate = self
         feedScroller.contentSize = feedImage.image!.size
+        messageOriginalCenter = messageView.center
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // The onCustomPan: method will be defined in Step 3 below.
+        var panGestureRecognizer = UIPanGestureRecognizer(target: self, action: "onMessagePan:")
+        
+        // Attach it to a view of your choice. If it's a UIImageView, remember to enable user interaction
+        view.addGestureRecognizer(messagePan)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func onMessagePan(panGestureRecognizer: UIPanGestureRecognizer) {
+        
+        // Absolute (x,y) coordinates in parent view
+        var point = panGestureRecognizer.locationInView(view)
+        
+        // Relative change in (x,y) coordinates from where gesture began.
+        var translation = panGestureRecognizer.translationInView(view)
+        var velocity = panGestureRecognizer.velocityInView(view)
+        
+        if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+            print("Gesture began at: \(point)")
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+            print("Gesture changed at: \(point)")
+            
+            messageView.center = CGPoint(x: messageOriginalCenter.x + translation.x, y: messageOriginalCenter.y) // allow drag on x-axis only
+            
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
+            print("Gesture ended at: \(point)")
+            
+            messageView.center = messageOriginalCenter // snap back to origin
+        }
+    }
 
 }
 
